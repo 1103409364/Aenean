40e57e654f2879ae51a1e40235c1a447f5a93c3f
1103409364 <1103409364@qq.com>
2019-07-31 10:23:35 +0800

    init

---
+++ src/components/header/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    header
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+
+</style>
