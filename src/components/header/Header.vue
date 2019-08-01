3ba0e84419b0e0b61b8e73c2961f1acc14ef88e2
1103409364 <1103409364@qq.com>
2019-08-01 14:02:02 +0800

    add popup

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -32,19 +32,41 @@
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background" :style="styleObj"/>
-    <div v-show="detailShow" class="detail">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <div class="star-wrapper">
-            <star :size="48" :score="seller.score"></star>
+    <!-- 弹出层添加动画 -->
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="item in seller.supports" :key="item.type">
+                <span class="icon" :class="classMap[item.type]"></span>
+                <span class="text">{{item.description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
           </div>
         </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
+        </div>
       </div>
-      <div class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -80,6 +102,9 @@ export default {
   methods: {
     showDetail () {
       this.detailShow = true
+    },
+    hideDetail () {
+      this.detailShow = false
     }
   },
   created () {
@@ -126,6 +151,7 @@ export default {
           .name
             margin-left 6px
             font-size 16px
+            font-weight 700
             line-height 18px
         .description
           margin-bottom 10px
@@ -215,6 +241,14 @@ export default {
       height 100%
       overflow auto
       background: rgba(7, 17, 27, 0.8)
+      // 弹出层渐变动画
+      &.fade-enter,
+      &.fade-leave-to
+        opacity: 0 // class被移除时,从0到1
+      &.fade-enter-active,
+      &.fade-leave-active //过程中都有这个class,在这里加过渡
+        transition: opacity .3s
+      backdrop-filter blur(10px)
       .detail-wrapper
         width 100%
         min-height 100%
@@ -227,6 +261,60 @@ export default {
             text-align center
             font-sizt 16px
             font-weight 700
+          .star-wrapper
+            margin-top 18px
+            padding 2px 0
+            text-align center
+          .title
+            display flex
+            width 80%
+            margin 28px auto 24px auto
+            .line
+              flex 1
+              position relative
+              top -6px
+              border-bottom 1px solid rgba(255, 255, 255, 0.2)
+            .text
+              padding 0 12px
+              font-size 14px
+              font-weight 700
+          .supports
+            width 80%
+            margin 0 auto
+            .support-item
+              padding 0 12px
+              margin-bottom 12px
+              font-size 0
+              &:last-child
+                margin-bottom 0
+              .icon
+                display inline-block
+                width 16px
+                height 16px
+                vertical-align top
+                margin-right 6px
+                background-size 16px 16px
+                background-repeat no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                line-height 16px
+                font-size 12px
+          .bulletin
+            width 80%
+            margin 0 auto
+            .content
+              padding 0 12px
+              line-height 24px
+              font-size 12px
       .detail-close
         position relative
         width 32px
