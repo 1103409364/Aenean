456c390f479e2e76154aa9973ec22f6c5e83da4c
1103409364 <1103409364@qq.com>
2019-08-05 16:10:50 +0800

    ratings overview

--- src/components/star/Star.vue
+++ src/components/star/Star.vue
@@ -62,7 +62,7 @@ export default {
       .star-item
         width 20px
         height 20px
-        margin-right 22px
+        margin-right 9px
         background-size 20px 20px
         &:last-child
           margin-right 0
@@ -76,7 +76,7 @@ export default {
        .star-item
         width 15px
         height 15px
-        margin-right 16px
+        margin-right 6px
         background-size 15px 15px
         &:last-child
           margin-right 0
